f1c68294de373131e1c29e8f6ad5b22abee2dcdb
Jelmer Groeneveld <jelmer@fluxility.com>
2017-11-28 13:44:14 +0100

    Added announcements

---
+++ src/pages/announcements/announcement.ts
@@ -0,0 +1,8 @@
+export class Announcement {
+  id: number;
+  title: string;
+  content: string;
+  image: any;
+  publishDate: Date;
+
+}
--- src/pages/announcements/announcements.ts
+++ src/pages/announcements/announcements.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Announcement } from "./announcement";
 
 @Component({
   selector: 'page-announcements',
@@ -7,8 +8,38 @@ import { NavController } from 'ionic-angular';
 })
 export class AnnouncementsPage {
 
+  private announcements : Announcement[];
+
   constructor(public navCtrl: NavController) {
 
+    this.getAnnouncements();
+
+  }
+
+  getAnnouncements () {
+    this.announcements = [
+      {
+        id: 0,
+        title: 'Nieuwe app!',
+        content: 'EmploView, de nieuwe alles in 1 app.',
+        image: 'assets/imgs/new-app.png',
+        publishDate: new Date()
+      },
+      {
+        id: 1,
+        title: 'Werknemers gezocht',
+        content: 'Er worden nieuwe werknemers gezogd. Iedereen met nieuwe contacten is welken.',
+        image: 'assets/imgs/employee.jpg',
+        publishDate: new Date()
+      },
+      {
+        id: 2,
+        title: 'Nieuw kantoor',
+        content: 'Binnenkort gaan we verhuizen.',
+        image: 'assets/imgs/move.jpeg',
+        publishDate: new Date()
+      }
+    ];
   }
 
 }
