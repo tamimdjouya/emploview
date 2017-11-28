42237c3cb2284289c1bc3deff13902ba7a3b367a
Jelmer Groeneveld <jelmer@fluxility.com>
2017-11-28 12:05:30 +0100

    Created tabs

---
+++ src/pages/hrm/hrm.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-hrm',
+  templateUrl: 'hrm.html'
+})
+export class HrmPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  }
+}
--- src/pages/intranet/intranet.ts
+++ src/pages/intranet/intranet.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-intranet',
+  templateUrl: 'intranet.html'
 })
-export class HomePage {
+export class IntranetPage {
 
   constructor(public navCtrl: NavController) {
 
